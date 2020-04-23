f34ca6e5f4306db233c69ba0de09183e37a3a87f
Chernyshelly <andreycherni@gmail.com>
2020-04-24 00:40:17 +0300

    Project created, file reading tested

---
+++ ASharpParcer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace ASharpParcer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string path = "d:/Git/test.txt";
+            StreamReader sr = new StreamReader(path);
+            Console.WriteLine(sr.ReadLine());
+            Console.WriteLine(sr.ReadLine());
+            sr.Close();
+        }
+    }
+}

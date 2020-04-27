e7a90a1ca1f2fafce9edc6266f4b7481ad2f8fce
Chernyshelly <andreycherni@gmail.com>
2020-04-28 01:21:41 +0300

    Some savings added

--- ASharpParcer/Program.cs
+++ ASharpParcer/Program.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace ASharpParcer
 {
     class Program
     {
+        public List<string> Lines = new List<string>();
+
+        public List<long> Vars = new List<long>();
+
+        public List<JumpPoint> JumpPlaces = new List<JumpPoint>();
+
+        public struct JumpPoint
+        {
+            public int lineId;
+            public string pointName;
+        }
+
+        public void ReadExecLine(int i)
+        {
+
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
